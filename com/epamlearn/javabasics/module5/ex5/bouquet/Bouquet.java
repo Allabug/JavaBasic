619842784acfef49139bbcbf00bc1ec18ffe94d2
Alla Jukova <alla-bugx@yandex.ru>
2020-12-06 21:27:40 +0300

    refactoring

--- com/epamlearn/javabasics/module5/ex5/bouquet/Bouquet.java
+++ com/epamlearn/javabasics/module5/ex5/bouquet/Bouquet.java
@@ -1,9 +1,8 @@
 package com.epamlearn.javabasics.module5.ex5.bouquet;
 
 import com.epamlearn.javabasics.module5.ex5.flowers.Flower;
-import lesson_inheritance.PriceCatalog;
 
-import java.math.BigDecimal;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +10,7 @@ public class Bouquet {
 
     private List<Flower> flowersForBouquet = new ArrayList<>();
     private Wrap wrap;
-    PriceCatalog priceCatalog = new PriceCatalog();
+
 
     public List<Flower> getFlowersForBouquet() {
         return flowersForBouquet;
